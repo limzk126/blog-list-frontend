74e1705364c8215f09603dd183de4c8bf2cf86a6
puzzlehead <dylanlim12698@gmail.com>
2022-08-10 23:59:28 +0800

    Fix typo

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ const App = () => {
         <div>
           {user.username} logged in <button onClick={onLogout}>logout</button>
         </div>
-        <BlogForm user={''} />
+        <BlogForm user={user} />
         <div>
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
